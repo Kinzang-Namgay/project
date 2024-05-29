4022a4429e54cc9a3736424533fb19779b3457db
Kinzang-Namgay <knamgay58@gmail.com>
2024-05-29 09:00:37 +0500

    Add files via upload

---
+++ final GeSar Project/contact.js
@@ -0,0 +1,8 @@
+var dataName = document.getElementById("name");
+var dataDate = document.getElementById("contact");
+var dataEmail = document.getElementById("email");
+
+
+function btn() {
+  alert("Thank you for messaging")
+}
